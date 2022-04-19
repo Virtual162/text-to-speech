fbca91f0b4889453a41153e1a1aaea3642d259d5
Virtual162 <69114235+Virtual162@users.noreply.github.com>
2022-04-20 01:00:56 +0900

    amended code to add file reading and verification
    need to check lines 14 to 18 and configure lines 21 and beyond to
    1. allow reading data from Text column of csv
    2. file name of output audio file must correspond with  filename in AudioFileName

--- tts.py
+++ tts.py
@@ -1,4 +1,6 @@
-import json
+pip install ibm-watson
+pip install pandas
+import json, pandas as pd
 from ibm_watson import TextToSpeechV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
@@ -8,6 +10,13 @@ with open("./auth.json",'r') as auth:
     auth_info = json.load(auth)
     apikey= auth_info['apikey']
     url=auth_info['url']
+
+#Text-to-speech向けのCSVファイルの読み取り
+TTS_Dataset = pd.read_csv("filepath or URL")
+TTS_TextData= pd.DataFrame(data=TTS_Dataset, columns=['Text', 'AudioFileName'])
+if TTS_TextData['Text'].str == True:
+    print ("テキストデータに文字以外の物はありません")　#Text only contains str values
+
 # 音声に変換したい文章
 text='ちなみに、先ほど探していたのは、こちらの資料でしょう。'
 
@@ -17,6 +26,10 @@ tts = TextToSpeechV1(authenticator=authenticator)
 tts.set_service_url(url)
 
 # 音声ファイル作成
-with open('./audio.mp3', 'wb') as audio_file:
+# Windowsでは完成してファイルはユーザーの/Docments/text-to-speech-main/フォルダーに保存されます。
+with open('./audio.mp3', 'wb') as audio_file: 　# audio file name should match the name in the \
+    # 'AudioFileName' column for each row of data in the 'text column'
     res = tts.synthesize(text, accept='audio/mp3', voice='ja-JP_EmiV3Voice').get_result()
     audio_file.write(res.content)
+    print("全てのオーディオファイルはユーザーの/Docments/text-to-speech-main/フォスターに保存されています。")
+
